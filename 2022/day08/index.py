561f4bc40fe1a4dc1b0f7324fc3bbea862c18e11
Lars Zondag <Lars.L.Zondag@shell.com>
2022-12-08 09:29:31 +0100

    Format day 08

--- 2022/day08/index.py
+++ 2022/day08/index.py
@@ -2,45 +2,49 @@
 import numpy as np
 
 with open("input.txt", "r", encoding="utf-8") as f:
-    data = [list(map(int, s)) for s in f.read().splitlines()]
-data = np.array(data)
+    data_list = [list(map(int, s)) for s in f.read().splitlines()]
+data = np.array(data_list)
 
 #%% Part 1
 visible = 0
 
 J, I = data.shape
-for j in range(1, J-1):
-    for i in range(1, I-1):
+for j in range(1, J - 1):
+    for i in range(1, I - 1):
         height = data[j, i]
         if (height > data[:j, i]).all():
-            visible+=1
-        elif (height > data[j+1:, i]).all():
-            visible+=1
+            visible += 1
+        elif (height > data[j + 1 :, i]).all():
+            visible += 1
         elif (height > data[j, :i]).all():
-            visible+=1
-        elif (height > data[j, i+1:]).all():
-            visible+=1
-        
-visible += 2 * (J - 2) + 2*(I-2) + 4
+            visible += 1
+        elif (height > data[j, i + 1 :]).all():
+            visible += 1
+
+visible += 2 * (J - 2) + 2 * (I - 2) + 4
 print("Part 1: ", visible)
 #%% Part 2
 max_view_score = 0
-for j in range(1, J-1):
-    for i in range(1, I-1):
+for j in range(1, J - 1):
+    for i in range(1, I - 1):
         height = data[j, i]
 
         larger_left = height > data[j, :i][::-1]
-        view_left = np.logical_and.accumulate(larger_left).sum() + (~larger_left).any() 
-        
-        larger_right = height > data[j, i+1:]
-        view_right = np.logical_and.accumulate(larger_right).sum() + (~larger_right).any() 
-        
+        view_left = np.logical_and.accumulate(larger_left).sum()
+        view_left += (~larger_left).any()
+
+        larger_right = height > data[j, i + 1 :]
+        view_right = np.logical_and.accumulate(larger_right).sum()
+        view_right += (~larger_right).any()
+
         larger_top = height > data[:j, i][::-1]
-        view_top = np.logical_and.accumulate(larger_top).sum() + (~larger_top).any()
-        
-        larger_bottom = height > data[j+1:, i]
-        view_bottom = np.logical_and.accumulate(larger_bottom).sum() + (~larger_bottom).any() 
-        
+        view_top = np.logical_and.accumulate(larger_top).sum()
+        view_top += (~larger_top).any()
+
+        larger_bottom = height > data[j + 1 :, i]
+        view_bottom = np.logical_and.accumulate(larger_bottom).sum()
+        view_bottom += (~larger_bottom).any()
+
         view_score = view_left * view_right * view_top * view_bottom
         max_view_score = max(max_view_score, view_score)
 print("Part 2: ", max_view_score)
