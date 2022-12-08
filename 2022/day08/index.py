96dd6ecbfeafebe9ab4f22f80c04e9e0847a68a1
Lars Zondag <Lars.L.Zondag@shell.com>
2022-12-08 09:26:59 +0100

    Clean up day 8

--- 2022/day08/index.py
+++ 2022/day08/index.py
@@ -9,33 +9,38 @@ data = np.array(data)
 visible = 0
 
 J, I = data.shape
-for i in range(1, J-1):
-    for j in range(1, I-1):
-        height = data[i, j]
-        if (height > data[:i, j]).all():
+for j in range(1, J-1):
+    for i in range(1, I-1):
+        height = data[j, i]
+        if (height > data[:j, i]).all():
             visible+=1
-        elif (height > data[i+1:, j]).all():
+        elif (height > data[j+1:, i]).all():
             visible+=1
-        elif (height > data[i, :j]).all():
+        elif (height > data[j, :i]).all():
             visible+=1
-        elif (height > data[i, j+1:]).all():
+        elif (height > data[j, i+1:]).all():
             visible+=1
         
 visible += 2 * (J - 2) + 2*(I-2) + 4
 print("Part 1: ", visible)
 #%% Part 2
 max_view_score = 0
-for i in range(1, J-1):
-    for j in range(1, I-1):
-        height = data[i, j]
-        larger_left = height > data[i, :j][::-1]
+for j in range(1, J-1):
+    for i in range(1, I-1):
+        height = data[j, i]
+
+        larger_left = height > data[j, :i][::-1]
         view_left = np.logical_and.accumulate(larger_left).sum() + (~larger_left).any() 
-        larger_right = height > data[i, j+1:]
+        
+        larger_right = height > data[j, i+1:]
         view_right = np.logical_and.accumulate(larger_right).sum() + (~larger_right).any() 
-        larger_top = height > data[:i, j][::-1]
+        
+        larger_top = height > data[:j, i][::-1]
         view_top = np.logical_and.accumulate(larger_top).sum() + (~larger_top).any()
-        larger_bottom = height > data[i+1:, j]
+        
+        larger_bottom = height > data[j+1:, i]
         view_bottom = np.logical_and.accumulate(larger_bottom).sum() + (~larger_bottom).any() 
+        
         view_score = view_left * view_right * view_top * view_bottom
         max_view_score = max(max_view_score, view_score)
 print("Part 2: ", max_view_score)
