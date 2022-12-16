9fabdb3af0ebd71f1dc9e504600b46ceb18120c3
Lars Zondag <Lars.L.Zondag@shell.com>
2022-12-16 09:12:38 +0100

    Update day 15

--- 2022/day15/index.py
+++ 2022/day15/index.py
@@ -1,89 +1,75 @@
 #%%
+from numba import jit
+
 # file = "sample.txt"
 file = "input.txt"
 with open(file, "r", encoding="utf-8") as f:
     data = [s[len("Sensor at "):].split(": closest beacon is at ") for s in f.read().splitlines()]
 
 sensors = []
+beacons = []
 min_x, max_x = 0, 0
 min_y, max_y = 0, 0
 for sensor_line in data:
-    sensor = {}
     string_x, string_y = sensor_line[0].split(", ")
-    sensor["x"] = int(string_x[2:])
-    sensor["y"] = int(string_y[2:])
+    sensor_x = int(string_x[2:])
+    sensor_y = int(string_y[2:])
     string_x, string_y = sensor_line[1].split(", ")
-    sensor["beacon_x"] = int(string_x[2:])
-    sensor["beacon_y"] = int(string_y[2:])
+    beacon_x = int(string_x[2:])
+    beacon_y = int(string_y[2:])
 
-    sensor["distance"] = abs(sensor["x"] - sensor["beacon_x"]) + abs(sensor["y"] - sensor["beacon_y"])
-    sensors.append(sensor)
-    min_x = min(min_x, sensor["x"] - sensor["distance"])
-    max_x = max(max_x, sensor["x"] + sensor["distance"])
-    min_y = min(min_y, sensor["y"] - sensor["distance"])
-    max_y = max(max_y, sensor["y"] + sensor["distance"])
+    distance = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
+    sensors.append((sensor_x, sensor_y, distance))
+    beacons.append((beacon_x, beacon_y))
+    min_x = min(min_x, sensor_x - distance)
+    max_x = max(max_x, sensor_x + distance)
+    min_y = min(min_y, sensor_y - distance)
+    max_y = max(max_y, sensor_y + distance)
+
 #%% Part 1
-debug = False
-def debug_print(*args, **kwargs):
-    if debug:
-        print(*args, **kwargs)
-
 y = 10 if file == "sample.txt" else 2000000
+
 n_cannot_contain_beacon = 0
-print(f"x to check: ", max_x - min_x)
+
 for x in range(min_x, max_x + 1):
     cannot_contain_beacon = False
     is_sensor_position = False
     is_beacon_position = False
-    for sensor in sensors:
-        if x == sensor["x"] and y == sensor["y"]:
+    for (sensor_x, sensor_y, sensor_distance) in sensors:
+        if x == sensor_x and y == sensor_y:
             is_sensor_position = True
             break
-        if x == sensor["beacon_x"] and y == sensor["beacon_y"]:
-            is_beacon_position = True
-            break
-        distance = abs(sensor["x"] - x) + abs(sensor["y"] - y)
-        if distance <= sensor["distance"]:
+        distance = abs(sensor_x - x) + abs(sensor_y - y)
+        if distance <= sensor_distance:
             cannot_contain_beacon = True
             break
     if is_sensor_position:
-        debug_print("S", end="")
-    elif is_beacon_position:
-        debug_print("B", end="")
-    elif cannot_contain_beacon:
-        n_cannot_contain_beacon += 1
-        debug_print("#", end="")
-    else:
-        debug_print(".", end="")
-debug_print("")
-n_cannot_contain_beacon
+        continue
+    if cannot_contain_beacon :
+        is_beacon_position = any(beacon_x == x and beacon_y == y for beacon_x, beacon_y in beacons)
+        if not is_beacon_position:
+            n_cannot_contain_beacon += 1
+    
 
+print("Part 1: ", n_cannot_contain_beacon)
 
 
 # %% Part 2
+@jit
+def find_beacon(start, end, sensor_data):
+    for y in range(start, end + 1):
+        x = 0
+        while x <= end + 1:
+            this_is_the_beacon = True
+            for sensor_x, sensor_y, sensor_distance in sensor_data:
+                distance = abs(x-sensor_x) + abs(y-sensor_y)
+                if distance <= sensor_distance:
+                    x = sensor_x + sensor_distance - abs(sensor_y - y) + 1
+                    this_is_the_beacon = False
+                    break
+            if this_is_the_beacon:
+                print("Part 2: ", x * 4000000 + y)
+                return
 
-sns = [(sensor["x"], sensor["y"], sensor["distance"]) for sensor in sensors]
-
-min_x = 0
-max_x = 20 if file == "sample.txt" else 4000000
-min_y = 0
-max_y = 20 if file == "sample.txt" else 4000000
-
-for x in range(min_x, max_x + 1):
-    y = 0
-    this_is_the_beacon = True
-    if x % 100000 == 0:
-        print(f"{x} out of {max_x}")
-    while y <= max_y:
-        this_is_the_beacon = True
-        for sensor_x, sensor_y, sensor_distance in sns:
-            distance = abs(x-sensor_x) + abs(y-sensor_y)
-            if distance <= sensor_distance:
-                y = sensor_y + sensor_distance - abs(sensor_x - x) + 1
-                this_is_the_beacon = False
-                break
-        if this_is_the_beacon:
-            print(x, y)
-            print(x * 4000000 + y)
-            break
-    if this_is_the_beacon: break
+find_beacon(0, 20 if file == "sample.txt" else 4000000, sensors)
+# %%
